981cf8224746726e54e197550c0e0b32be48a184
texastony <5892063+texastony@users.noreply.github.com>
2021-11-29 09:57:11 -0800

    fix(uint32ArrayFrom): increment index and polyfill for Uint32Array instead of Array

--- packages/util/src/uint32ArrayFrom.ts
+++ packages/util/src/uint32ArrayFrom.ts
@@ -3,11 +3,12 @@
 
 // IE 11 does not support Array.from, so we do it manually
 export function uint32ArrayFrom(a_lookUpTable: Array<number>): Uint32Array {
-  if (!Array.from) {
+  if (!Uint32Array.from) {
     const return_array = new Uint32Array(a_lookUpTable.length)
     let a_index = 0
     while (a_index < a_lookUpTable.length) {
       return_array[a_index] = a_lookUpTable[a_index]
+      a_index += 1
     }
     return return_array
   }
